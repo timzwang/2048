fbb57c4aa3759af0a14a2d521d0269c8a821ca59
Timothy Wang <timothy.wang@zefr.com>
2014-03-20 13:26:28 -0700

    Simple change for 3x3 and 256 win

--- js/game_manager.js
+++ js/game_manager.js
@@ -139,8 +139,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 256 tile
+          if (merged.value === 256) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
